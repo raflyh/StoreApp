d5def36d83ed6acc8c9cd15eb7fe47cb8ba7b61f
Farrah Arrazy <farraharrazyrazy@gmail.com>
2022-04-26 23:50:44 +0700

    menambah model dan DbContext pada ShippingService

---
+++ ShippingService/Models/Shipping.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShippingService.Models
+{
+    public class Shipping
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string ShippingStatus { get; set; }
+        [Required]
+        public int InVoiceId { get; set; }
+        public InVoice inVoice { get; set; }
+    }
+}
