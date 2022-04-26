6aa3a68aec87e4c65998e57037a1ab3434e8a01a
Farrah Arrazy <farraharrazyrazy@gmail.com>
2022-04-27 04:27:07 +0700

    menambah interface dan repo pada shipping service

---
+++ ShippingService/Data/IShipping.cs
@@ -0,0 +1,20 @@
+﻿using ShippingService.Models;
+
+namespace ShippingService.Data
+{
+    public interface IShipping
+    {
+        bool SaveChange();
+
+        //InVoice
+        IEnumerable<InVoice> GetAllInVoices();
+        void CreateInVoice(InVoice inVo);
+        bool InVoiceExist(int inVoiceId);
+        bool ExternalInVoiceExist(int externalInVoiceId);
+
+        //Shipping
+        Shipping GetShippingForInVoice(int inVoiceId);
+        Shipping GetShipping(int inVoiceId, int shippingId);
+        void CreateShipping(int inVoiceId, Shipping shipping);
+    }
+}
---
+++ ShippingService/Data/ShippingRepo.cs
@@ -0,0 +1,61 @@
+﻿using ShippingService.Models;
+
+namespace ShippingService.Data
+{
+    public class ShippingRepo : IShipping
+    {
+        private readonly AppDbContext _context;
+
+        public ShippingRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateInVoice(InVoice inVo)
+        {
+            if (inVo == null)
+                throw new ArgumentNullException(nameof(inVo));
+            _context.inVoices.Add(inVo);
+        }
+
+        public void CreateShipping(int inVoiceId, Shipping shipping)
+        {
+            if (shipping == null)
+                throw new ArgumentNullException(nameof(shipping));
+
+            shipping.InVoiceId = inVoiceId;
+            _context.Shippings.Add(shipping);
+        }
+
+        public bool ExternalInVoiceExist(int externalInVoiceId)
+        {
+            return _context.inVoices.Any(p => p.ExternalID == externalInVoiceId);
+        }
+
+        public IEnumerable<InVoice> GetAllInVoices()
+        {
+            return _context.InVoices.ToList();
+        }
+
+        public Shipping GetShipping(int inVoiceId, int shippingId)
+        {
+            return _context.Shippings.Where(c => c.InVoiceId == inVoiceId &&
+                c.Id == shippingId).FirstOrDefault();
+        }
+
+        public Shipping GetShippingForInVoice(int inVoiceId)
+        {
+            return _context.Shippings.FirstOrDefault(c => c.InVoiceId == inVoiceId);
+        }
+
+        public bool InVoiceExist(int inVoiceId)
+        {
+            return _context.inVoices.Any(p => p.Id == inVoiceId);
+        }
+
+        public bool SaveChange()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
