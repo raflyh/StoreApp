a9a3f82e72ffa3a61ae791895667da769549f534
ElfSinaga <elfridals12@gmail.com>
2022-04-27 05:31:39 +0800

    new Sync

--- OrderService/Model/InVoice.cs
+++ OrderService/Model/InVoice.cs
@@ -1,10 +1,17 @@
-﻿namespace OrderService.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderService.Model
 {
     public class InVoice
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string CodeInvoice { get; set; }
+        [Required]
         public int TotalCost { get; set; }
+        [Required]
         public DateTime Date { get; set; }
         public string OrderStatus { get; set; }
         public List<Product> Products { get; set; }
---
+++ OrderService/SyncDataService/HttpProductDataClient.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderService.SyncDataService
+{
+    public class HttpProductDataClient : IProductDataClient
+    {
+        public Task SendProductToInVoice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
