b95754bf2d52aa2dbb8df15db21217c159329610
ElfSinaga <elfridals12@gmail.com>
2022-04-27 00:29:41 +0800

    new

---
+++ OrderService/Model/InVoice.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderService.Model
+{
+    public class InVoice
+    {
+        public int Id { get; set; }
+        public int TotalCost { get; set; }
+        public DateTime Date { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
