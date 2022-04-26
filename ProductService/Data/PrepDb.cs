2f8bfb9451f6738d3510b2a23a452c1f54fee17e
Rafly H <raflyh75@gmail.com>
2022-04-26 23:21:04 +0700

    Menambahkan Repository, Interface, Models

---
+++ ProductService/Data/PrepDb.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
+
+namespace ProductService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateAsyncScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProd);
+            }
+        }
+
+        private static void SeedData(AppDbContext? appDbContext, bool isProd)
+        {
+            if (isProd)
+            {
+                Console.WriteLine("--> Migrating --");
+                try
+                {
+                    appDbContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Migrate Failed {ex.Message} --");
+                }
+            }
+
+            if (!appDbContext.Products.Any())
+            {
+                Console.WriteLine("--> Seeding Data --");
+                var products = new List<Product>()
+                {
+                    new Product() { Name = "Gundam Model X", Price = 500.000, Categories = new List<Category>() 
+                        { 
+                            new Category() { Name = "Toys" }, 
+                            new Category(){ Name = "Hobby" } 
+                        }
+                    },
+                    new Product() { Name = "Pet Cargo", Price = 150.000, Categories = new List<Category>()
+                        {
+                            new Category() { Name = "Pets"},
+                            new Category(){ Name = "Items"}
+                        }
+                    }
+                };
+                products.ForEach(p => appDbContext.Products.AddRange(p));
+                appDbContext.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> Data Already Exist --");
+            }
+        }
+    }
+}
---
+++ ProductService/Interface/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using ProductService.Models;
+
+namespace ProductService.Interface
+{
+    public interface IProductRepo
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetById(int id);
+        void Add(Product product);
+        void Remove(int id);
+        bool Update(Product product);
+        bool SaveChanges();
+    }
+}
