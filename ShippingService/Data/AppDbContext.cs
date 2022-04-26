70c8bb783440f25c5c86f1ee2819767c33dac9d6
Farrah Arrazy <farraharrazyrazy@gmail.com>
2022-04-26 22:12:52 +0700

    Menambah Model dan DbContext

---
+++ ShippingService/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShippingService.Models;
+
+namespace ShippingService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<InVoice> inVoices { get; set; }
+        public DbSet<Shipping> Shippings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ ShippingService/Models/InVoice.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShippingService.Models
+{
+    public class InVoice
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public int ExternalID { get; set; }
+        public Shipping Shipping { get; set; }
+    }
+}
