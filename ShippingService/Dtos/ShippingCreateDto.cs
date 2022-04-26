9df5f7239adb6ee2060f2aac10d43e5965240fae
Haekal Akmal Tsaqif <akmaltsaqif70@gmail.com>
2022-04-27 05:24:38 +0700

    Update DTO Shipping

--- ShippingService/Dtos/ShippingCreateDto.cs
+++ ShippingService/Dtos/ShippingCreateDto.cs
@@ -5,8 +5,6 @@ namespace ShippingService.Dtos
     public class ShippingCreateDto
     {
         [Required]
-        public int Id { get; set; }
-        [Required]
         public string ShippingStatus { get; set; }
         [Required]
         public int InVoiceId { get; set; }
