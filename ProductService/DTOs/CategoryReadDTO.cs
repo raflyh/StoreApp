bb65cd24b79f1823e8f803e22021647426172dbd
Rafly H <raflyh75@gmail.com>
2022-04-27 15:30:25 +0700

    added DTOs, Interface, Profiles

---
+++ ProductService/DTOs/CategoryReadDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductService.DTOs
+{
+    public class CategoryReadDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ProductReadDTO> Products { get; set; }
+    }
+}
--- ProductService/Interface/IProductRepo.cs
+++ ProductService/Interface/IProductRepo.cs
@@ -5,7 +5,6 @@ namespace ProductService.Interface
     public interface IProductRepo
     {
         IEnumerable<Product> GetAllProducts();
-        IEnumerable<Category> GetAllCategories();
         Product GetById(int id);
         void Add(Product product);
         void Remove(int id);
