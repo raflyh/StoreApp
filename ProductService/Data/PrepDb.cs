5d8a6ef5adf32c85582fc16325beb1bdfb39cc6f
Rafly H <raflyh75@gmail.com>
2022-04-27 14:31:03 +0700

    change in prepdb

--- ProductService/Data/PrepDb.cs
+++ ProductService/Data/PrepDb.cs
@@ -31,11 +31,11 @@ namespace ProductService.Data
             if (!context.Products.Any())
             {
                 Console.WriteLine("--> Seeding Data --");
-                var products = new List<Product>();
+                //var products = new List<Product>();
                 var categories = new List<Category>()
                 {
-                    new Category() { Id = 1, Name = "Toys", Products = new List<Product>() 
-                        { 
+                    new Category() { Id = 1, Name = "Toys", Products = new List<Product>()
+                        {
                             new Product() { Id = 1, Name = "Gundam Model X", Price = 500.000},
                             new Product() { Id = 2, Name = "Tamiya Model Z", Price = 250.000}
                         }
@@ -59,8 +59,25 @@ namespace ProductService.Data
                         }
                     }
                 };
-                products.ForEach(p => context.Products.AddRange(p));
-                categories.ForEach(p => context.Categories.AddRange(p));
+                //products.ForEach(p => context.Products.AddRange(p));
+                //categories.ForEach(p => context.Categories.AddRange(p));
+                context.Categories.AddRange(
+                    new Category(){ Id = 1, Name = "Toys" },
+                    new Category(){ Id = 2, Name = "Pets" },
+                    new Category(){ Id = 3, Name = "Music" },
+                    new Category(){ Id = 4, Name = "Pets" }
+                    );
+                context.SaveChanges();
+                context.Products.AddRange(
+                    new Product() { Id = 1, Name = "Gundam Model X", Price = 500.000 , CategoryId = 1},
+                    new Product() { Id = 2, Name = "Tamiya Model Z", Price = 250.000 , CategoryId = 1},
+                    new Product() { Id = 3, Name = "Pet Collar", Price = 50.000, CategoryId = 2 },
+                    new Product() { Id = 4, Name = "Cat Food 1Kg", Price = 250.000, CategoryId = 2 },
+                    new Product() { Id = 5, Name = "Guitar Strings", Price = 125.000, CategoryId = 3 },
+                    new Product() { Id = 6, Name = "Drum Stick", Price = 80.000, CategoryId = 3 },
+                    new Product() { Id = 7, Name = "Pet Collar", Price = 50.000, CategoryId = 4 },
+                    new Product() { Id = 8, Name = "Cat Food 1Kg", Price = 250.000, CategoryId = 4 }
+                    );
                 context.SaveChanges();
             }
             else
--- ProductService/Models/Product.cs
+++ ProductService/Models/Product.cs
@@ -1,9 +1,15 @@
-﻿namespace ProductService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductService.Models
 {
     public class Product
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Price { get; set; }
         public Category Category { get; set; }
         public int CategoryId { get; set; }
