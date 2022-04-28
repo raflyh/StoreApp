52261a899d6277059b2d27c8efd7a1ffd996aae1
Haekal Akmal Tsaqif <akmaltsaqif70@gmail.com>
2022-04-28 16:59:19 +0700

    update InvoiceRepo

--- OrderService/Model/Product.cs
+++ OrderService/Model/Product.cs
@@ -7,6 +7,7 @@ namespace OrderService.Model
         [Key]
         [Required]
         public int Id { get; set; }
+        public string Name {get; set;}
         public int ExternalId { get; set; }
         public InVoice InVoice { get; set; }
     }
